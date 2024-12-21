384b324273ef3a080ae15560a0879794acb361f1
Ariel Tecay <arieltecay@gmail.com>
2024-12-21 20:10:23 -0300

    Add files via upload

---
+++ routes/transactionRouter.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const transactionCtrl = require('../controllers/transactionCtrl');
+const isAuth = require('../middlewares/isAuth');
+
+const transactionRouter = express.Router();
+transactionRouter.post('/api/v1/transaction/create', isAuth, transactionCtrl.create);
+transactionRouter.get('/api/v1/transaction/lists', isAuth, transactionCtrl.getFilteredTransactions);
+transactionRouter.get('/api/v1/transaction/totalAmount', isAuth, transactionCtrl.getTotalAmount);
+transactionRouter.get('/api/v1/transaction/all', isAuth, transactionCtrl.lists);
+transactionRouter.put('/api/v1/transaction/update/:id', isAuth, transactionCtrl.update);
+transactionRouter.delete('/api/v1/transaction/delete/:id', isAuth, transactionCtrl.delete);
+transactionRouter.get('/api/v1/transaction/print/:id', isAuth, transactionCtrl.print);
+
+module.exports = transactionRouter;
