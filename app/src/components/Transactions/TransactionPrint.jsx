18aacbef09ca34b99c08840033b3e99d00978999
arieltecay <arieltecay@gmail.com>
2024-12-27 15:19:07 -0300

    feat: replace loading spinner with ClipLoader and format transaction amount

--- app/src/components/Transactions/TransactionPrint.jsx
+++ app/src/components/Transactions/TransactionPrint.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { printTransactionAPI } from "../../services/transactions/transactionService";
 import { useParams } from "react-router-dom";
+import { ClipLoader } from "react-spinners";
 
 const TransactionPrint = () => {
     const { id } = useParams();
@@ -11,9 +12,7 @@ const TransactionPrint = () => {
     });
     if (isLoading) {
         return (
-            <div className="flex justify-center items-center h-full">
-                <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-blue-500"></div>
-            </div>
+            <ClipLoader size={50} color={"#123abc"} loading={true} />
         );
     }
     const date = new Date(transaction.date);
@@ -22,7 +21,7 @@ const TransactionPrint = () => {
     return (
         <div className="card shadow-lg rounded-lg w-auto">
             <div className="card-body">
-                <h5 className="card-title">Monto: {transaction.amount}</h5>
+                <h5 className="card-title">Monto: $ {transaction?.amount.toLocaleString()}</h5>
                 <h6 className="card-subtitle mb-2 text-muted">Categoría: {transaction.category}</h6>
                 <h6 className="card-subtitle mb-2 text-muted">Descripción: {transaction.description}</h6>
                 <h6 className="card-subtitle mb-2 text-muted">Tipo: {transaction.type}</h6>
