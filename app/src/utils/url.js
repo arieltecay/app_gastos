a3d533b8e1ab22b457e4ed9723315df249583a71
arieltecay <arieltecay@gmail.com>
2025-06-08 21:04:58 -0300

    fix: update production base URL to point to the correct endpoint

--- app/src/utils/url.js
+++ app/src/utils/url.js
@@ -1,2 +1,2 @@
 export const BASE_URL = 'http://localhost:3000/api/v1';
-export const BASE_URL_PROD='https://api-gastos-nine.vercel.app/api/v1'
+export const BASE_URL_PROD='https://app-gastos-sigma.vercel.app/api/v1'
