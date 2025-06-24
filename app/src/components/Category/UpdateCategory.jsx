6f4724cdf8b31504c8b4c3acb047b774ba3d0198
arieltecay <arieltecay@gmail.com>
2025-06-23 23:46:30 -0300

    fix: improve loading state handling in UpdateCategory component

--- app/src/components/Category/UpdateCategory.jsx
+++ app/src/components/Category/UpdateCategory.jsx
@@ -65,87 +65,89 @@ const UpdateCategory = () => {
     }
   }, [categoryData]);
 
-  if (isLoadingCategory) {
-    return <AlertMessage type="info" message="Loading category data..." />;
-  }
 
   return (
-    <form
-      onSubmit={formik.handleSubmit}
-      className="max-w-lg mx-auto my-10 bg-white p-6 rounded-lg shadow-lg space-y-6"
-    >
-      <div className="text-center">
-        <h2 className="text-2xl font-semibold text-gray-800">
-          Update Category
-        </h2>
-        <p className="text-gray-600">Fill in the details below.</p>
-      </div>
-      {/* Display alert message */}
-      {isPending && <AlertMessage type="info" message="Updating..." />}
-      {isError && (
-        <AlertMessage
-          type="error"
-          message={
-            error?.response?.data?.message ||
-            "Something happened please try again later"
-          }
-        />
-      )}
-      {isSuccess && (
-        <AlertMessage
-          type="success"
-          message="Category updated successfully, redirecting..."
-        />
-      )}
-      {/* Category Type */}
-      <div className="space-y-2">
-        <label
-          htmlFor="type"
-          className="flex gap-2 items-center text-gray-700 font-medium"
+    <>
+      {isLoadingCategory ? <AlertMessage type="loading" message="Loading" />
+        :
+        <form
+          onSubmit={formik.handleSubmit}
+          className="max-w-lg mx-auto my-10 bg-white p-6 rounded-lg shadow-lg space-y-6"
         >
-          <FaWallet className="text-blue-500" />
-          <span>Type</span>
-        </label>
-        <select
-          {...formik.getFieldProps("type")}
-          id="type"
-          className="w-full p-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50"
-        >
-          <option value="">Select transaction type</option>
-          <option value="income">Income</option>
-          <option value="expense">Expense</option>
-        </select>
-        {formik.touched.type && formik.errors.type && (
-          <p className="text-red-500 text-xs">{formik.errors.type}</p>
-        )}
-      </div>
+          <div className="text-center">
+            <h2 className="text-2xl font-semibold text-gray-800">
+              Update Category
+            </h2>
+            <p className="text-gray-600">Fill in the details below.</p>
+          </div>
+          {/* Display alert message */}
+          {isPending && <AlertMessage type="info" message="Updating..." />}
+          {isError && (
+            <AlertMessage
+              type="error"
+              message={
+                error?.response?.data?.message ||
+                "Something happened please try again later"
+              }
+            />
+          )}
+          {isSuccess && (
+            <AlertMessage
+              type="success"
+              message="Category updated successfully, redirecting..."
+            />
+          )}
+          {/* Category Type */}
+          <div className="space-y-2">
+            <label
+              htmlFor="type"
+              className="flex gap-2 items-center text-gray-700 font-medium"
+            >
+              <FaWallet className="text-blue-500" />
+              <span>Type</span>
+            </label>
+            <select
+              {...formik.getFieldProps("type")}
+              id="type"
+              className="w-full p-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50"
+            >
+              <option value="">Select transaction type</option>
+              <option value="income">Income</option>
+              <option value="expense">Expense</option>
+            </select>
+            {formik.touched.type && formik.errors.type && (
+              <p className="text-red-500 text-xs">{formik.errors.type}</p>
+            )}
+          </div>
 
-      {/* Category Name */}
-      <div className="flex flex-col">
-        <label htmlFor="name" className="text-gray-700 font-medium">
-          <SiDatabricks className="inline mr-2 text-blue-500" />
-          Name
-        </label>
-        <input
-          type="text"
-          {...formik.getFieldProps("name")}
-          placeholder="Name"
-          id="name"
-          className="w-full mt-1 border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50 py-2 px-3"
-        />
-        {formik.touched.name && formik.errors.name && (
-          <p className="text-red-500 text-xs italic">{formik.errors.name}</p>
-        )}
-      </div>
+          {/* Category Name */}
+          <div className="flex flex-col">
+            <label htmlFor="name" className="text-gray-700 font-medium">
+              <SiDatabricks className="inline mr-2 text-blue-500" />
+              Name
+            </label>
+            <input
+              type="text"
+              {...formik.getFieldProps("name")}
+              placeholder="Name"
+              id="name"
+              className="w-full mt-1 border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50 py-2 px-3"
+            />
+            {formik.touched.name && formik.errors.name && (
+              <p className="text-red-500 text-xs italic">{formik.errors.name}</p>
+            )}
+          </div>
 
-      {/* Submit Button */}
-      <button
-        type="submit"
-        className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition-colors duration-200 transform"
-      >
-        Update Category
-      </button>
-    </form>
+          {/* Submit Button */}
+          <button
+            type="submit"
+            className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition-colors duration-200 transform"
+          >
+            Update Category
+          </button>
+        </form>
+      }
+    </>
   );
 };
 
